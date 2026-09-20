0db943992471ca699008305a2191c1596d4736bd
adminSajjad <yosefi1988@gmail.com>
2026-09-20 09:51:43 +0330

    runable

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -17,33 +17,41 @@ namespace WebApplicationStoreAdmin.Controllers
             var targetDate = DateTime.Now.AddDays(daysToAdd);
 
             //User Count
-            ViewBag.UserCount = getUserCount();
+            //ViewBag.UserCount = getUserCount();
+            ViewBag.UserCount = 100;
 
             //Transaction Count
-            var TransactionCountInLastSevenenDay = db.SD_Transactions
-                                    .Where(x => x.PaymentDate <= targetDate)
-                                    .ToList();
-            ViewBag.TransactionCountInLastSevenenDay = TransactionCountInLastSevenenDay
-                                    .Count;
-            ViewBag.TransactionCount = db.SD_Transactions.Count();
+            //var TransactionCountInLastSevenenDay = db.SD_Transactions
+            //                        .Where(x => x.PaymentDate <= targetDate)
+            //                        .ToList();
+            //ViewBag.TransactionCountInLastSevenenDay = TransactionCountInLastSevenenDay.Count;
+            ViewBag.TransactionCountInLastSevenenDay = 200;
+
+            //ViewBag.TransactionCount = db.SD_Transactions.Count();
+            ViewBag.TransactionCount = 300;
 
 
             //Transaction Amount
-            var TransactionAmountInLastSevenenDay = db.SD_Transactions
-                                    .Where(x => x.PaymentDate <= targetDate)
-                                    .ToList();
-            ViewBag.TransactionAmountInLastSevenenDay = TransactionAmountInLastSevenenDay
-                                    .Sum(x => x.SumShoppingBasketPrice);
-            ViewBag.TransactionAmount = db.SD_Transactions
-                                    .Sum(x => x.SumShoppingBasketPrice);
+            //var TransactionAmountInLastSevenenDay = db.SD_Transactions
+            //                        .Where(x => x.PaymentDate <= targetDate)
+            //                        .ToList();
+            //ViewBag.TransactionAmountInLastSevenenDay = TransactionAmountInLastSevenenDay
+            //                        .Sum(x => x.SumShoppingBasketPrice);
+
+            ViewBag.TransactionAmountInLastSevenenDay = 400;
+
+            //ViewBag.TransactionAmount = db.SD_Transactions.Sum(x => x.SumShoppingBasketPrice);
+            ViewBag.TransactionAmount = 500;
 
             //Object Counts
-            ViewBag.RemainingObjectCount = db.SD_ProductChargesProperties
-                                    .Sum(x=> x.RemainingCount);
+            //ViewBag.RemainingObjectCount = db.SD_ProductChargesProperties
+            //                        .Sum(x=> x.RemainingCount);
+            ViewBag.RemainingObjectCount = 600;
 
+            //ViewBag.AllBuyObjectCount = db.SD_ProductCharges 
+            //                        .Sum(x => x.BuyCount);
 
-            ViewBag.AllBuyObjectCount = db.SD_ProductCharges 
-                                    .Sum(x => x.BuyCount);
+            ViewBag.AllBuyObjectCount = 700;
             return View();
         }
         public ActionResult Index2()
@@ -67,11 +75,13 @@ namespace WebApplicationStoreAdmin.Controllers
 
         private dynamic getUserCount()
         {
-            return db.SD_Users.Count();
+            //return db.SD_Users.Count();
+            return 52;
         }
         private dynamic getASPUserCount()
         {
-            return db.AspNetUsers.Count();
+            //return db.AspNetUsers.Count();
+            return 100;
         }
 
         
--- obj/Release/Package/PackageTmp/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="WebApplicationStoreAdmin.MvcApplication" Language="C#" %>
